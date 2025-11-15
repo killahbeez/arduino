b76c6f0dee8fc6ab817a9ff44bbec3b3214692ee
Adrian Carstea <adrian.carstea@gmail.com>
2025-11-15 09:47:00 +0200

    varianta fara MQTT, si fara updatare leduri cand nu returneazace trebuie, cand script.google.com nu merge

--- esp8266/esp8266_check_gmail/esp8266_check_gmail.ino
+++ esp8266/esp8266_check_gmail/esp8266_check_gmail.ino
@@ -3,7 +3,6 @@
 #include <WiFiClientSecureBearSSL.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
-#include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
 #include <Ticker.h>
 #include <ArduinoJson.h>
@@ -14,20 +13,12 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, D2, NEO_GRB + NEO_KHZ800);
 const char* ssid = "UPCD476AA2";
 const char* password = "Fr4vebAeepuk";
 
-IPAddress ip(192, 168, 0, 122);
+/*IPAddress ip(192, 168, 0, 122);
 IPAddress gateway(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 0);
+IPAddress subnet(255, 255, 255, 0);*/
 
-/* MQTT Settings */
-//std::unique_ptr<BearSSL::WiFiClientSecure> wificlient(new BearSSL::WiFiClientSecure());
 BearSSL::WiFiClientSecure wificlient;
-
-
 WiFiClient wificlientNOSSL;
-PubSubClient client(wificlientNOSSL);
-
-IPAddress broker(192, 168, 0, 16);       // Address of the MQTT broker
-#define CLIENT_ID "client-gmail" // Client ID to send to the broker
 
 const char* host = "script.google.com";
 const int httpsPort = 443;
@@ -36,104 +27,7 @@ const char *GScriptId = "AKfycbwCPTk8AKsi2lMBSooBb2sD8F7moVpN-co3scUSc_MhTlf-Xs6
 
 // echo | openssl s_client -connect script.google.com:443 |& openssl x509 -fingerprint -noout
 // SHA1 fingerprint of the certificate
-/*const char* fingerprint = "2C:0B:CF:F3:2B:F7:F0:76:D4:B1:1F:DB:E2:21:58:B5:EC:B7:6A:FF";
-
-const char* root_ca PROGMEM = R"EOF(
-        -----BEGIN CERTIFICATE-----
-        MIINhzCCDS6gAwIBAgIRANVXCHkKVuT7ErpqC4E8o+AwCgYIKoZIzj0EAwIwOzEL
-        MAkGA1UEBhMCVVMxHjAcBgNVBAoTFUdvb2dsZSBUcnVzdCBTZXJ2aWNlczEMMAoG
-        A1UEAxMDV0UyMB4XDTI1MTAxMzA4Mzc0NloXDTI2MDEwNTA4Mzc0NVowFzEVMBMG
-        A1UEAwwMKi5nb29nbGUuY29tMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1l22
-        CpbQywaaPnyQ2BKjQL3vkkV52/28sUO+gq4hxEkkD5kmhK8s7J/95ugE7Okjj+oi
-        mEeom+ZVjfE1NZMNIKOCDDUwggwxMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAK
-        BggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBSd0+bmm/WgOrvdwD6d
-        v821J6HxFzAfBgNVHSMEGDAWgBR1vsR3ron2RDd9z7FoHx0a69w0WTBYBggrBgEF
-        BQcBAQRMMEowIQYIKwYBBQUHMAGGFWh0dHA6Ly9vLnBraS5nb29nL3dlMjAlBggr
-        BgEFBQcwAoYZaHR0cDovL2kucGtpLmdvb2cvd2UyLmNydDCCCgsGA1UdEQSCCgIw
-        ggn+ggwqLmdvb2dsZS5jb22CFiouYXBwZW5naW5lLmdvb2dsZS5jb22CCSouYmRu
-        LmRldoIVKi5vcmlnaW4tdGVzdC5iZG4uZGV2ghIqLmNsb3VkLmdvb2dsZS5jb22C
-        GCouY3Jvd2Rzb3VyY2UuZ29vZ2xlLmNvbYIYKi5kYXRhY29tcHV0ZS5nb29nbGUu
-        Y29tggsqLmdvb2dsZS5jYYILKi5nb29nbGUuY2yCDiouZ29vZ2xlLmNvLmlugg4q
-        Lmdvb2dsZS5jby5qcIIOKi5nb29nbGUuY28udWuCDyouZ29vZ2xlLmNvbS5hcoIP
-        Ki5nb29nbGUuY29tLmF1gg8qLmdvb2dsZS5jb20uYnKCDyouZ29vZ2xlLmNvbS5j
-        b4IPKi5nb29nbGUuY29tLm14gg8qLmdvb2dsZS5jb20udHKCDyouZ29vZ2xlLmNv
-        bS52boILKi5nb29nbGUuZGWCCyouZ29vZ2xlLmVzggsqLmdvb2dsZS5mcoILKi5n
-        b29nbGUuaHWCCyouZ29vZ2xlLml0ggsqLmdvb2dsZS5ubIILKi5nb29nbGUucGyC
-        CyouZ29vZ2xlLnB0gg8qLmdvb2dsZWFwaXMuY26CESouZ29vZ2xldmlkZW8uY29t
-        ggwqLmdzdGF0aWMuY26CECouZ3N0YXRpYy1jbi5jb22CD2dvb2dsZWNuYXBwcy5j
-        boIRKi5nb29nbGVjbmFwcHMuY26CEWdvb2dsZWFwcHMtY24uY29tghMqLmdvb2ds
-        ZWFwcHMtY24uY29tggxna2VjbmFwcHMuY26CDiouZ2tlY25hcHBzLmNughJnb29n
-        bGVkb3dubG9hZHMuY26CFCouZ29vZ2xlZG93bmxvYWRzLmNughByZWNhcHRjaGEu
-        bmV0LmNughIqLnJlY2FwdGNoYS5uZXQuY26CEHJlY2FwdGNoYS1jbi5uZXSCEiou
-        cmVjYXB0Y2hhLWNuLm5ldIILd2lkZXZpbmUuY26CDSoud2lkZXZpbmUuY26CEWFt
-        cHByb2plY3Qub3JnLmNughMqLmFtcHByb2plY3Qub3JnLmNughFhbXBwcm9qZWN0
-        Lm5ldC5jboITKi5hbXBwcm9qZWN0Lm5ldC5jboIXZ29vZ2xlLWFuYWx5dGljcy1j
-        bi5jb22CGSouZ29vZ2xlLWFuYWx5dGljcy1jbi5jb22CF2dvb2dsZWFkc2Vydmlj
-        ZXMtY24uY29tghkqLmdvb2dsZWFkc2VydmljZXMtY24uY29tghFnb29nbGV2YWRz
-        LWNuLmNvbYITKi5nb29nbGV2YWRzLWNuLmNvbYIRZ29vZ2xlYXBpcy1jbi5jb22C
-        EyouZ29vZ2xlYXBpcy1jbi5jb22CFWdvb2dsZW9wdGltaXplLWNuLmNvbYIXKi5n
-        b29nbGVvcHRpbWl6ZS1jbi5jb22CEmRvdWJsZWNsaWNrLWNuLm5ldIIUKi5kb3Vi
-        bGVjbGljay1jbi5uZXSCGCouZmxzLmRvdWJsZWNsaWNrLWNuLm5ldIIWKi5nLmRv
-        dWJsZWNsaWNrLWNuLm5ldIIOZG91YmxlY2xpY2suY26CECouZG91YmxlY2xpY2su
-        Y26CFCouZmxzLmRvdWJsZWNsaWNrLmNughIqLmcuZG91YmxlY2xpY2suY26CEWRh
-        cnRzZWFyY2gtY24ubmV0ghMqLmRhcnRzZWFyY2gtY24ubmV0gh1nb29nbGV0cmF2
-        ZWxhZHNlcnZpY2VzLWNuLmNvbYIfKi5nb29nbGV0cmF2ZWxhZHNlcnZpY2VzLWNu
-        LmNvbYIYZ29vZ2xldGFnc2VydmljZXMtY24uY29tghoqLmdvb2dsZXRhZ3NlcnZp
-        Y2VzLWNuLmNvbYIXZ29vZ2xldGFnbWFuYWdlci1jbi5jb22CGSouZ29vZ2xldGFn
-        bWFuYWdlci1jbi5jb22CGGdvb2dsZXN5bmRpY2F0aW9uLWNuLmNvbYIaKi5nb29n
-        bGVzeW5kaWNhdGlvbi1jbi5jb22CJCouc2FmZWZyYW1lLmdvb2dsZXN5bmRpY2F0
-        aW9uLWNuLmNvbYIWYXBwLW1lYXN1cmVtZW50LWNuLmNvbYIYKi5hcHAtbWVhc3Vy
-        ZW1lbnQtY24uY29tggtndnQxLWNuLmNvbYINKi5ndnQxLWNuLmNvbYILZ3Z0Mi1j
-        bi5jb22CDSouZ3Z0Mi1jbi5jb22CCzJtZG4tY24ubmV0gg0qLjJtZG4tY24ubmV0
-        ghRnb29nbGVmbGlnaHRzLWNuLm5ldIIWKi5nb29nbGVmbGlnaHRzLWNuLm5ldIIM
-        YWRtb2ItY24uY29tgg4qLmFkbW9iLWNuLmNvbYIZKi5nZW1pbmkuY2xvdWQuZ29v
-        Z2xlLmNvbYIUZ29vZ2xlc2FuZGJveC1jbi5jb22CFiouZ29vZ2xlc2FuZGJveC1j
-        bi5jb22CHiouc2FmZW51cC5nb29nbGVzYW5kYm94LWNuLmNvbYINKi5nc3RhdGlj
-        LmNvbYIUKi5tZXRyaWMuZ3N0YXRpYy5jb22CCiouZ3Z0MS5jb22CESouZ2NwY2Ru
-        Lmd2dDEuY29tggoqLmd2dDIuY29tgg4qLmdjcC5ndnQyLmNvbYIQKi51cmwuZ29v
-        Z2xlLmNvbYIWKi55b3V0dWJlLW5vY29va2llLmNvbYILKi55dGltZy5jb22CCmFp
-        LmFuZHJvaWSCC2FuZHJvaWQuY29tgg0qLmFuZHJvaWQuY29tghMqLmZsYXNoLmFu
-        ZHJvaWQuY29tggRnLmNuggYqLmcuY26CBGcuY2+CBiouZy5jb4IGZ29vLmdsggp3
-        d3cuZ29vLmdsghRnb29nbGUtYW5hbHl0aWNzLmNvbYIWKi5nb29nbGUtYW5hbHl0
-        aWNzLmNvbYIKZ29vZ2xlLmNvbYISZ29vZ2xlY29tbWVyY2UuY29tghQqLmdvb2ds
-        ZWNvbW1lcmNlLmNvbYIIZ2dwaHQuY26CCiouZ2dwaHQuY26CCnVyY2hpbi5jb22C
-        DCoudXJjaGluLmNvbYIIeW91dHUuYmWCC3lvdXR1YmUuY29tgg0qLnlvdXR1YmUu
-        Y29tghFtdXNpYy55b3V0dWJlLmNvbYITKi5tdXNpYy55b3V0dWJlLmNvbYIUeW91
-        dHViZWVkdWNhdGlvbi5jb22CFioueW91dHViZWVkdWNhdGlvbi5jb22CD3lvdXR1
-        YmVraWRzLmNvbYIRKi55b3V0dWJla2lkcy5jb22CBXl0LmJlggcqLnl0LmJlghph
-        bmRyb2lkLmNsaWVudHMuZ29vZ2xlLmNvbYITKi5hbmRyb2lkLmdvb2dsZS5jboIS
-        Ki5jaHJvbWUuZ29vZ2xlLmNughYqLmRldmVsb3BlcnMuZ29vZ2xlLmNughUqLmFp
-        c3R1ZGlvLmdvb2dsZS5jb20wEwYDVR0gBAwwCjAIBgZngQwBAgEwNgYDVR0fBC8w
-        LTAroCmgJ4YlaHR0cDovL2MucGtpLmdvb2cvd2UyL3h1enQzUFU5Rl93LmNybDCC
-        AQQGCisGAQQB1nkCBAIEgfUEgfIA8AB2ABaDLavwqSUPD/A6pUX/yL/II9CHS/YE
-        KSf45x8zE/X6AAABmdzu6SYAAAQDAEcwRQIgUbaGHlT0xnvcjqlVk4D59sPiCu1f
-        eS0z5pOn9ZqBm74CIQDZOa37lSBcFbWumMDyEzgzMxrpjFNbusm4l3dI+FVL2AB2
-        AA5XlLzzrqk+MxssmQez95Dfm8I9cTIl3SGpJaxhxU4hAAABmdzu6P0AAAQDAEcw
-        RQIgfnbgxYmgdomns1SKkwQr5ksFJ8RfAJyVnUSNQ8cWlTgCIQCyAo5BdGkLBZ/B
-        fCSLdpKuJ+3iqmny3SKF94k7DW7KqDAKBggqhkjOPQQDAgNHADBEAiAEBt+Rx2tH
-        /R8PKusV/1uThSH4WCXBGU6Qw99R74efxQIgY+/RWxYdtDMzx5RPtBEusAcTVddY
-        lGVecHDvi51K9kg=
-        -----END CERTIFICATE-----
-        )EOF";
-*/
-
-
-
-boolean restart = 1;
-boolean get_restart_cnt = 0;
-const char* topic_restart_cnt = "/home/check_email_restart_cnt";
-uint32_t cnt_restart = 0;
-/**
-   MQTT callback to process messages
-*/
-void callback(char* topic, byte* payload, unsigned int length) {
-  payload[length] = '\0';
-  if (strcmp(topic, topic_restart_cnt) == 0 ) {
-    cnt_restart = atoi((char*) payload);
-    get_restart_cnt = 1;
-  }
-}
-
+//const char* fingerprint = "2C:0B:CF:F3:2B:F7:F0:76:D4:B1:1F:DB:E2:21:58:B5:EC:B7:6A:FF";
 
 void setup() {
   Serial.begin(115200);
@@ -172,10 +66,6 @@ void setup() {
     Serial.println("MDNS responder started");
   }
 
-  /* Prepare MQTT client */
-  client.setServer(broker, 1883);
-  client.setCallback(callback);
-
   // Use HTTPSRedirect class to create TLS connection to Google Apps Script
   Serial.println("Try connecting to Google Script API");
   reconnectGoogleApps();
@@ -209,32 +99,6 @@ void setNeopixel(uint8_t nr_emails) {
   pixels.show();
 }
 
-
-
-/**
-   Attempt connection to MQTT broker and subscribe to command topic
-*/
-void reconnect() {
-  
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(CLIENT_ID)) {
-      Serial.println("connected");
-      client.subscribe(topic_restart_cnt);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-
-
 /**
    Attempt connection to Google Apps Script
 */
@@ -258,7 +122,7 @@ void reconnectGoogleApps() {
     }
     else{
       Serial.println("Connection failed. Retrying...");
-      delay(1000);
+      delay(500);
     }
   }
 
@@ -276,21 +140,6 @@ void reconnectGoogleApps() {
 volatile boolean tick = 0;
 void callback_ticker() {
   tick = 1;
-}
-
-String getResponse(WiFiClientSecure& client) {
-  String response = "";
-  
-  // Așteaptă până când clientul este conectat
-  while (client.connected()) {
-    // Citește fiecare caracter disponibil și adaugă-l la response
-    while (client.available()) {
-      char c = client.read();
-      response += c;  // Adaugă caracterul citit la răspuns
-    }
-  }
-  
-  return response;
 }
 
 String removeChunkedEncoding(String body) {
@@ -399,19 +248,10 @@ void loop() {
     Serial.println("WiFi connected");
   }
 
-  if (WiFi.status() == WL_CONNECTED) {
-
-    if (!client.connected()) {
-      reconnect();
-    }
-    
+  if (WiFi.status() == WL_CONNECTED) {    
     if (!wificlient.connected()) {
       reconnectGoogleApps();
     }
-  }
-
-  if (client.connected()) {
-    client.loop();
   }
 
   if (wificlient.connected() && tick) {
@@ -419,16 +259,6 @@ void loop() {
 
     //check for Unread Primary messages
     String urlUnreadPrimary =  String("/macros/s/") + GScriptId + "/exec?write=getUnreadPrimary&toCell=C3";
-    /*
-    wificlient.print("GET " + String(urlUnreadPrimary) + " HTTP/1.1\r\n");
-    wificlient.print("Host: " + String(host) + "\r\n");
-    wificlient.print("Connection: close\r\n\r\n");*/
-
-    /*StaticJsonBuffer<1000> jsonBufferUnreadPrimary;
-
-    String serverResponse = getResponse(wificlient);
-    Serial.println(serverResponse);  // Afișează răspunsul complet în Serial Monitor*/
-
     
     String serverResponse = httpGETFollowRedirect(host, urlUnreadPrimary);
     Serial.println(urlUnreadPrimary);
@@ -437,28 +267,33 @@ void loop() {
 
     StaticJsonBuffer<1000> jsonBufferUnreadPrimary;
     JsonObject& rootUnread = jsonBufferUnreadPrimary.parseObject(serverResponse);
-    Serial.println("threadsPrimary: " + String(rootUnread["threadsPrimary"]));
 
-    if (last_cnt_threads_unread_primary != rootUnread["threadsPrimary"]) {
-      if (rootUnread["threadsPrimary"] > 0) {
-        char buffer[100];
-        setNeopixel((int)rootUnread["threadsPrimary"]);
-
-        sprintf(buffer, "You have %d INBOX emails", (int)rootUnread["threadsPrimary"]);
-        client.publish("/home/debug", buffer);
+    if (rootUnread["threadsPrimary"].is<int>()) {
+      Serial.println("threadsPrimary: " + String(rootUnread["threadsPrimary"]));
+  
+      if (last_cnt_threads_unread_primary != rootUnread["threadsPrimary"]) {
+        if (rootUnread["threadsPrimary"] > 0) {
+          char buffer[100];
+          setNeopixel((int)rootUnread["threadsPrimary"]);
+          sprintf(buffer, "You have %d INBOX emails", (int)rootUnread["threadsPrimary"]);
+        }
+        else {
+          pixels.clear();
+          pixels.show();
+        }
+        last_cnt_threads_unread_primary = rootUnread["threadsPrimary"];
       }
-      else {
-        pixels.clear();
-        pixels.show();
-        client.publish("/home/debug", "NO new emails");
-      }
-      last_cnt_threads_unread_primary = rootUnread["threadsPrimary"];
+      Serial.println("##########################################");
+      Serial.println((int)rootUnread["threadsPrimary"]);
+      Serial.println("##########################################");
     }
-
-
-    Serial.println("##########################################");
-    Serial.println((int)rootUnread["threadsPrimary"]);
-    Serial.println("##########################################");
+    else{
+      Serial.println("##########################################");
+      Serial.println("          Nu updateaza ledurile,ca nu a returnat scriptul json-ul scontat diverse motive");
+      Serial.println("##########################################");
+      
+    }
+    
     tick = 0;
 
 
@@ -468,13 +303,5 @@ void loop() {
     digitalWrite(D5, LOW);
   }
   digitalWrite(D5,LOW);
-  
-  if (restart && get_restart_cnt) {
-    char buffer[30];
-    sprintf(buffer, "%d", ++cnt_restart);
-    client.publish(topic_restart_cnt, buffer, true);
-    restart = 0;
-  }
-  
 
 }
